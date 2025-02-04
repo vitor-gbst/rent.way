520a268828cce2b1b9b9635df4d1d342d0bb642b
Vitor Gabriel <vitor_gabsss@outlook.com>
2025-02-04 18:45:44 -0300

    nav quase pronto so falta a lista

---
+++ codigo/menu.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", function() {
+    let nav = document.querySelector('#nav');
+    let logo = document.querySelector('#logo');
+
+    let logopreta = "../imagens/rent_way_preto.png";
+    let logobranca = "../imagens/rent.way_branco.png";
+
+    if (window.scrollY > 400) {
+        nav.classList.add('roll-nav');
+        logo.src = logopreta;
+    } else {
+        nav.classList.remove('roll-nav');
+        logo.src = logobranca;
+    }
+});
